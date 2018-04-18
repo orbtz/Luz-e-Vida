f8144d00aa20e3f068bfecb69ad052c59a981f56
Fábio Rafael Barbosa <fabio-fabio_fabio@hotmail.com>
2018-04-18 19:19:20 -0300

    Update de telas em construção Cada bloco preenchido com uma cor

--- PIC_2018/Resources/Resource.Designer.cs
+++ PIC_2018/Resources/Resource.Designer.cs
@@ -41,11 +41,82 @@ namespace PIC_2018
 			}
 		}
 		
-		public partial class Layout
+		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Main = 2130837504;
+			public const int b01 = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int b02 = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int b03 = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int b04 = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int b05 = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int logo = 2130837509;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050001
+			public const int button01 = 2131034113;
+			
+			// aapt resource value: 0x7f050004
+			public const int button02 = 2131034116;
+			
+			// aapt resource value: 0x7f050003
+			public const int button03 = 2131034115;
+			
+			// aapt resource value: 0x7f050005
+			public const int button04 = 2131034117;
+			
+			// aapt resource value: 0x7f050006
+			public const int buttonMainObjetivos = 2131034118;
+			
+			// aapt resource value: 0x7f050000
+			public const int imageView1 = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int textView1 = 2131034114;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
+		public partial class Layout
+		{
+			
+			// aapt resource value: 0x7f030000
+			public const int Main = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Objetivos = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int StartLayout = 2130903042;
 			
 			static Layout()
 			{
@@ -60,8 +131,8 @@ namespace PIC_2018
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int app_name = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int app_name = 2130968576;
 			
 			static String()
 			{
